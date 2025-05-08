f44434bc8aba083bedfa32ae49d7c6f028d7a75d
zaiets-maksim <zaiets.maksim@gmail.com>
2025-05-09 01:26:48 +0300

    refactor project location and hierarchy + removed useless scripts

--- Assets/Scripts/Extensions/LazyExtensions.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public static class LazyExtensions
-{
-    public static T AsLazy<T>(this T instance) where T : class
-    {
-        return new Lazy<T>(() => instance).Value;
-    }
-}
